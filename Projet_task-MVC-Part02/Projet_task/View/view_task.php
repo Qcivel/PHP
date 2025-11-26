823c1c7850a57b04d479409259b55a20873bbf30
Qcivel <q.civel@yahoo.fr>
2025-11-26 17:27:06 +0100

    Push

--- Projet_task-MVC-Part02/Projet_task/View/view_task.php
+++ Projet_task-MVC-Part02/Projet_task/View/view_task.php
@@ -3,10 +3,12 @@
 
 <h1>Mes taches</h1>
 
-<form action="">
-    <label for="task">Tache à ajouter :</label>
-    <input type="text" name="task" id="task">
+<form action="" method="post">
+    <label for="name_task">Tache à ajouter :</label>
+    <input type="text" name="name_task" id="name_task">
+    <label for="content_task" name="content_task" id="content_task" >Contenu :</label>
+    <input type="text" name="content_task" id="content_task">
+    <label for="date_task" >Date de la tache :</label>
+    <input type="date" name="date_task" id="date_task">
     <input type="submit" name="addTask" value="Ajouter">
 </form>
-
-
--- Projet_task-MVC-Part02/Projet_task/task.php
+++ Projet_task-MVC-Part02/Projet_task/task.php
@@ -1,13 +1,42 @@
 <?php
-//Démarrer la session
+// Démarrer la session
 session_start();
 
-//Variable d'affichage
+include './Model/model_task.php';
+
+// Variable d'affichage
+$message = '';
 $title = 'Mon Compte Utilisateur';
 
+// Connexion à la BDD
+$bdd = new PDO('mysql:host=localhost;dbname=task', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+// Récupérer l'id de l'utilisateur depuis la session
+$id_user = isset($_SESSION['id_user']) ? (int)$_SESSION['id_user'] : null;
+
+// Vérifier si le formulaire a été soumis
+if (isset($_POST['addTask'])) {
+    if (!empty($_POST['name_task']) && !empty($_POST['content_task']) && !empty($_POST['date_task'])) {
+        $name_task = htmlentities(stripslashes(strip_tags(trim($_POST['name_task']))));
+        $content_task = htmlentities(stripslashes(strip_tags(trim($_POST['content_task']))));
+        $date_task = $_POST['date_task'] . ' 00:00:00';
+
+        if ($id_user) {
+            $message = createTask($bdd, $name_task, $content_task, $date_task, $id_user);
+
+            
+        } else {
+            $message = 'Utilisateur non connecté.';
+        }
+    } else {
+        $message = 'Veuillez remplir tous les champs.';
+    }
+}
+
+// Inclure les vues
 include './View/header.php';
-
 include './View/view_task.php';
-
 include './View/footer.php';
+echo "Mes taches: $name_task $content_task $date_task";
+
 ?>
